413220d7f2dcf32262b187a8617b00b51c3dcec6
Shrishailya345 <karandeshrishailya@gmail.com>
2024-09-01 11:43:23 +0530

    rusarpay key added

--- db.js
+++ db.js
@@ -1,7 +1,7 @@
 const mongoose=require("mongoose")
-//require('dotenv').config()
-//DATABASE=process.env.DATABASEURL
-const url='mongodb+srv://Shrishailya15:LQ3s4TtWUMSIXmbS@cluster0.qhlcqqy.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
+require('dotenv').config()
+DATABASE=process.env.DATABASEURL
+const url=DATABASE
 module.exports.connect=()=>{
     mongoose.connect(url,console.log("Database is connected"))
 }
